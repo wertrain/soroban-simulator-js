1150082769e81adefaed72ba3b74f4303ea3d303
wertrain <wertrain@gmail.com>
2020-01-11 17:56:33 +0900

    rewrite looks like React code

--- src/index.js
+++ src/index.js
@@ -3,223 +3,46 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Soroban extends React.Component {
-    render() {
-      return (
-        <table>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td class="separater"><div class="tama icon"></div></td>
-            <td class="separater"><div class="tama icon"></div></td>
-            <td class="separater"><div class="tama icon"></div></td>
-            <td class="separater"><div class="tama icon"></div></td>
-            <td class="separater"><div class="tama icon"></div></td>
-            <td class="separater"><div class="tama icon"></div></td>
-            <td class="separater"><div class="tama icon"></div></td>
-            <td class="separater"><div class="tama icon"></div></td>
-            <td class="separater"><div class="tama icon"></div></td>
-            <td class="separater"><div class="tama icon"></div></td>
-            <td class="separater"><div class="tama icon"></div></td>
-            <td class="separater"><div class="tama icon"></div></td>
-            <td class="separater"><div class="tama icon"></div></td>
-            <td class="separater"></td>
-          </tr>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td><div class="tama icon"></div></td>
-            <td></td>
-          </tr>
-        </table>
-      )
+  cell(props) {
+    const tamaClass = (props.tama ? "tama icon" : null)
+    const separaterClass = (props.sep ? "separater" : null)
+    return (
+      <td class={separaterClass}>
+        <div class={tamaClass}></div>
+      </td>
+    );
+  }
+  cells(props) {
+    const td = (this.cell({
+      tama:props.tama,
+      sep:props.sep
+    }));
+    
+    let tds = [];
+    for(let i=0; i < props.num - 1; i++) {
+        tds.push(td);
     }
-}
-
-class SorobanTama extends React.Component {
-  render() {
-    return (<div class="tama icon"></div>)
-  }
-}
-
-class SorobanRaw extends React.Component {
-  render() {
-    return (
-      <tr>
-        <td>{this.props.value}</td>
-        <td>{this.props.value}</td>
-        <td>{this.props.value}</td>
-        <td>{this.props.value}</td>
-        <td>{this.props.value}</td>
-        <td>{this.props.value}</td>
-        <td>{this.props.value}</td>
-        <td>{this.props.value}</td>
-        <td>{this.props.value}</td>
-        <td>{this.props.value}</td>
-        <td>{this.props.value}</td>
-        <td>{this.props.value}</td>
-        <td>{this.props.value}</td>
-        <td>{this.props.value}</td>
-        <td></td>
-      </tr>
-    )
-  }
-}
-
-class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
+    
+    tds.push(this.cell({
+      tama:false,
+      sep:props.sep
+    }))
+    return (<tr>{tds}</tr>);
   }
   render() {
     return (
-      <button
-      className="square"
-      onClick={() => this.setState({value: 'X'})}
-    >
-      {this.state.value}
-    </button>
+      <table>
+        { this.cells({num:14}) }
+        { this.cells({num:14, sep:true, tama:true}) }
+        { this.cells({num:14}) }
+        { this.cells({num:14, tama:true}) }
+        { this.cells({num:14, tama:true}) }
+        { this.cells({num:14, tama:true}) }
+        { this.cells({num:14, tama:true}) }
+      </table>
     );
   }
 }
-  
-class Board extends React.Component {
-  renderSquare(i) {
-      return <Square value={i} />;
-  }
-
-  render() {
-    const status = 'Next player: X';
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
-
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
-
-// ========================================
 
 ReactDOM.render(
   //<Game />,
