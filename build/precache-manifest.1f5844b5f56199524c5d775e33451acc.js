0352f7fcf16bf5d759ed77fdb056b852b1fbe24b
wertrain <wertrain@gmail.com>
2020-01-13 23:09:30 +0900

    add build app

---
+++ build/precache-manifest.1f5844b5f56199524c5d775e33451acc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b433ba39936989721cf3f4bab2ee43b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a75e9aca53d6b3aa2a87",
+    "url": "/static/css/main.46aac1f8.chunk.css"
+  },
+  {
+    "revision": "575f5abd8043f3daac3b",
+    "url": "/static/js/2.427bbd6a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.427bbd6a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a75e9aca53d6b3aa2a87",
+    "url": "/static/js/main.f40fadf5.chunk.js"
+  },
+  {
+    "revision": "e566721526d6e15b6c3b",
+    "url": "/static/js/runtime-main.6a79928a.js"
+  }
+]);
